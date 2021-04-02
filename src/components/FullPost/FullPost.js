e7bd73c0b7c5f802831e2a8fbe5d3d7ba5698ce8
Your Name <you@example.com>
2021-04-02 01:52:52 -0700

    added new styles

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,36 +1,9 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import Comments from "../Comments/Comments";
-// import classes from "./FullPost.module.css";
-// const FullPost = ({ id }) => {
-//   const [post, setPost] = useState({
-//     title: "Loading...",
-//     body: "Wait for it!",
-//   });
-//   useEffect(() => {
-//     axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-//       .then((response) => {
-//         console.log(response);
-//         const post = response.data;
-//         setPost(post);
-//       });
-//   }, [id]);
-//   return (
-//     <main className={classes.FullPost}>
-//       <h1>{post.title}</h1>
-//       <p>{post.body}</p>
-//       <Comments postId={id} />
-//     </main>
-//   );
-// }
-// export default FullPost;
-
 
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Comments from "../Comments/Comments";
 import classes from "./FullPost.module.css";
-import Author from "./Author/Author"
+import Author from "../Author/Author"
 const FullPost = ({ id }) => {
   const [post, setPost] = useState({
     title: "Loading...",
@@ -56,3 +29,36 @@ const FullPost = ({ id }) => {
   );
 }
 export default FullPost;
+
+
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import Coments from "../Coments/Coments";
+// import classes from "./FullPost.module.css";
+// import Author from "../Author/Author";
+
+// const FullPost = ({ id }) => {
+//   const [post, setPost , SetuserId] = useState({
+//     title: "Loading...",
+//     body: "Wait for it!",
+//     userId:0
+//   });
+//   useEffect(() => {
+//     axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+//       .then((response) => {
+//         console.log(response);
+//         const post = response.data;
+//         setPost(post);
+//       });
+//   }, [id]);
+
+//   return (
+//     <div className={classes.FullPost}>
+//       <h1>{post.title}</h1>
+//      <p>{post.body}</p>
+//      <Coments postId={id} />
+//     <Author userId={ post.userId } />
+//     </div>
+//   );
+// }
+// export default FullPost;
