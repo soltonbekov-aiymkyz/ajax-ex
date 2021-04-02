1030ff99797e14df4070824161c57abcee363224
Your Name <you@example.com>
2021-04-02 01:47:43 -0700

    added new styles

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,11 +1,28 @@
 
+
+import axios from "axios";
+
+import { useEffect, useState } from "react";
 import classes from "./Author.module.css";
 const Author = ({ userId }) =>{
-   
+    
+  const [email, setEmail] = useState (
+    "loading..."
+  );
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+
+      .then((response) => {
+        const email = response.data.email;
+        setEmail(email);
+      });
+  }, [userId]);
+
 
   return (
     <div className={classes.Author}>
-  {userId}
+  <div>{email}</div>
     </div>
   );
   }
