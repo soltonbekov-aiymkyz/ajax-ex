60d8914926c02e49940e7682b3949d52f83b357d
Your Name <you@example.com>
2021-04-02 07:00:53 -0700

    corected files

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,43 +1,11 @@
 
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Comments from "../Comments/Comments";
-import classes from "./FullPost.module.css";
-import Author from "../Author/Author"
-const FullPost = ({ id }) => {
-  const [post, setPost] = useState({
-    title: "Loading...",
-    body: "Wait for it!",
-    userId:0
-  });
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-      .then((response) => {
-        console.log(response);
-        const post = response.data;
-        setPost(post);
-      });
-  }, [id]);
-  return (
-    <main className={classes.FullPost}>
-      <h1>{post.title}</h1>
-      <Author  userId={ post.userId } />
-      <p>{post.body}</p>
-      <Comments postId={id} />
-    </main>
-  );
-}
-export default FullPost;
-
-
 // import axios from "axios";
 // import { useEffect, useState } from "react";
-// import Coments from "../Coments/Coments";
+// import Comments from "../Comments/Comments";
 // import classes from "./FullPost.module.css";
-// import Author from "../Author/Author";
-
+// import Author from "../Author/Author"
 // const FullPost = ({ id }) => {
-//   const [post, setPost , SetuserId] = useState({
+//   const [post, setPost] = useState({
 //     title: "Loading...",
 //     body: "Wait for it!",
 //     userId:0
@@ -50,14 +18,46 @@ export default FullPost;
 //         setPost(post);
 //       });
 //   }, [id]);
-
 //   return (
-//     <div className={classes.FullPost}>
+//     <main className={classes.FullPost}>
 //       <h1>{post.title}</h1>
-//      <p>{post.body}</p>
-//      <Coments postId={id} />
-//     <Author userId={ post.userId } />
-//     </div>
+//       <Author  userId={ post.userId } />
+//       <p>{post.body}</p>
+//       <Comments postId={id} />
+//     </main>
 //   );
 // }
 // export default FullPost;
+
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Author from "../Author/Author";
+import classes from "./FullPost.module.css";
+
+const FullPost = ({ id }) => {
+
+    const[post, setPost] = useState({
+        title: "",
+        userId: "",
+        body: "",
+    })
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then((response) => {
+                const post = response.data;
+            setPost(post)
+        });
+    }, [id]);
+    
+    return ( 
+        <main className = {classes.FullPost}>
+            <h1>{post.title}</h1>
+            <Author userid = {post.userId}/>
+            <p>{post.body}</p>
+        </main>
+    );
+}
+ 
+export default FullPost;
