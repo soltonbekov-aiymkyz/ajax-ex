fbab70074347cbbbe9e1d42833dfe50d5782f322
Your Name <you@example.com>
2021-04-02 06:59:43 -0700

    sdfgH

--- src/App.js
+++ src/App.js
@@ -33,25 +33,34 @@
 // }
 // export default App;
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import FullPost from './components/FullPost/FullPost'
+import Albums from './components/Albums/Albums';
 import Comments from './components/Comments/Comments';
-import Posts from './components/Posts/Posts';
+import FullComment from './components/FullComment/FullComment';
+import FullPost from './components/FullPost/FullPost';
 import Pictures from './components/Pictures/Pictures';
+import Posts from './components/Posts/Posts';
+
 function App() {
   const [selectedPost, setSelectedPost] = useState(0);
-  console.log("[App.js] render");
+  const [selectedAlbum, setSelectedAlbum] = useState(0);
+
+  let fullPost = "Select a post!";
+  if (selectedPost == 0) {
+    fullPost = <FullPost id = {selectedPost} />
+  }
+
   return (
     <div className="App">
-      {/* <button onClick={() => setMessage("Bye")}>Toggle</button> */}
       <Posts setSelectedPost={setSelectedPost} />
-      {selectedPost}
-       <Pictures albumId={albums}   />
-
-
-
+      <Comments setSelectedPost={setSelectedPost} />
+      <FullPost id = {selectedPost}/>
+      <FullComment id = {selectedPost} />
+      <Albums setSelectedAlbum = {setSelectedAlbum}/>
+      <Pictures albumId = {selectedAlbum} />
     </div>
   );
 }
+
 export default App;
